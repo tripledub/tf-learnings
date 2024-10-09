1e4dd15e86001b2d5812ab61600441eac68148c9
Stewart Campbell <tripledub@users.noreply.github.com>
2024-10-09 10:52:47 +0000

    Adding providers

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+  owner = "tripledub"
+}
+
